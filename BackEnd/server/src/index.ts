74bf5218a7cc8778fbe2167ec381f195fd36e07d
himanshusinha <himanshusinha011@gmail.com>
2025-08-24 00:54:49 +0530

    updated 23/08

--- BackEnd/server/src/index.ts
+++ BackEnd/server/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+
+import Note, { NoteDocument } from "./models/notes"; // Importing the Note model
 import "./db"; // Importing the database connection
 // create a server
 const app = express();
@@ -21,13 +23,40 @@ app.post("/", (req, res) => {
   console.log(req.body);
   res.json({ message: "I am listening " });
 });
-
+interface IncomingBody {
+  title: string;
+  description?: string;
+}
 // create a post route
-app.post("/create", (req, res) => {
+app.post("/create", async (req, res) => {
   // here we need data so that we can create new note/todo
   // res.send("<h1>Hello World</h1>");
-  console.log(req.body);
+  const newNote = new Note<NoteDocument>({
+    title: (req.body as IncomingBody).title,
+    description: (req.body as IncomingBody).description,
+  });
+
+  await newNote.save();
   res.json({ message: "I am listening to create !!" });
+});
+
+//update note
+app.patch("/:noteId", async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    return res.json({ message: "Note not found!" });
+  }
+
+  const { title, description } = req.body as IncomingBody;
+  if (title) note.title = title;
+  if (description) note.description = description;
+
+  await note.save();
+  res.json({
+    note,
+  });
 });
 
 // listen to some port
